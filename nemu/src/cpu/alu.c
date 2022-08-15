fd1d2fe4eb5cd3d35b1c0cbd7871c858a657a635
cuikangyuan <cuikangyuan@outlook.com>
2022-08-15 15:47:34 +0800

    finish alu add instr

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -1,14 +1,143 @@
 #include "cpu/cpu.h"
 
+/*
+Carry Flag
+Set on high-order bit carry or borrow; cleared otherwise
+*/
+void set_CF_add(uint32_t result, uint32_t src, size_t data_size)
+{
+	//参考i386手册 sec 2.3.4.1
+	result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)), data_size);
+	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
+	cpu.eflags.CF = result < src;
+}
+
+/*
+Zero Flag ── Set if result is zero; cleared otherwise.
+*/
+void set_ZF(uint32_t result, size_t data_size)
+{
+	result = result & (0xFFFFFFFF >> (32 - data_size));
+	cpu.eflags.ZF = (result == 0);
+}
+
+/*
+Sign Flag ── Set equal to high-order bit of result (0 is
+positive, 1 if negative).
+*/
+void set_SF(uint32_t result, size_t data_size)
+{
+	result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)), data_size);
+	cpu.eflags.SF = sign(result);
+}
+
+/*
+Parity Flag ── Set if low-order eight bits of result contain
+an even number of 1 bits; cleared otherwise.
+*/
+void set_PF(uint32_t result)
+{
+	int even = 0;
+	switch (result & 0xf)
+	{
+	case 0x0:
+	case 0x3:
+	case 0x5:
+	case 0x6:
+	case 0x9:
+	case 0xa:
+	case 0xc:
+	case 0xf:
+		even = 0; //even num
+		break;
+	default:
+		even = 1; //odd num
+		break;
+	}
+	switch ((result >> 4) & 0xf)
+	{
+	case 0x0:
+	case 0x3:
+	case 0x5:
+	case 0x6:
+	case 0x9:
+	case 0xa:
+	case 0xc:
+	case 0xf:
+		if (even == 0)
+		{
+			cpu.eflags.PF = 1;
+		} else {
+			cpu.eflags.PF = 0;
+		}
+		break;
+	default:
+		if (even == 1)
+		{
+			cpu.eflags.PF = 1;
+		} else {
+			cpu.eflags.PF = 0;
+		}
+		break;
+	}
+
+
+}
+
+/*
+Overflow Flag ── Set if result is too large a positive number
+or too small a negative number (excluding sign-bit) to fit in
+destination operand; cleared otherwise.
+*/
+
+void set_OF(uint32_t result, uint32_t src, uint32_t dest, size_t data_size)
+{
+	switch (data_size)
+	{
+	case 8:
+		result = sign_ext(result & 0xFF, 8);
+		src = sign_ext(src & 0xFF, 8);
+		dest = sign_ext(dest & 0xFF, 8);
+		break;
+	case 16:
+		result = sign_ext(result & 0xFFFF, 16);
+		src = sign_ext(src & 0xFFFF, 16);
+		dest = sign_ext(dest & 0xFFFF, 16);
+		break;
+	default:
+		//nothing
+		break;
+	}
+	if (sign(src) == sign(dest))
+	{
+		if (sign(src) != sign(result))
+		{
+			cpu.eflags.OF = 1;
+		} else {
+			cpu.eflags.OF = 0;
+		}
+		
+	} else {
+		cpu.eflags.OF = 0;
+	}
+	
+}
+
 uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 {
 #ifdef NEMU_REF_ALU
 	return __ref_alu_add(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+	uint32_t res = 0;
+	res = dest + src;
+
+	set_CF_add(res, src, data_size);
+	set_PF(res);
+	// set_AF(); //不模拟
+	set_ZF(res, data_size);
+	set_SF(res, data_size);
+	set_OF(res, src, dest, data_size);
+	return res & (0xFFFFFFFF >> (32 - data_size));
 #endif
 }
 
